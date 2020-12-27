fd41b5cb39296c00411603ccc5733f3e690022be
bbodek <elik@example.com>
2020-12-27 21:27:06 +0200

    material io elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrontPageComponent } from './front-page/front-page.component';
@@ -23,7 +26,9 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 		AppRoutingModule,
 		FormsModule,
 		NoopAnimationsModule,
-		MatButtonModule
+		MatButtonModule,
+		MatFormFieldModule,
+		MatInputModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
